36ccf20fe83db60659255f46019ccea23314e1e2
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-13 16:03:04 +0700

    home: intro section

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,9 +1,11 @@
-import HeroSection from "./sections/hero";
+import HeroSection from "./sections/01-hero";
+import IntroSection from "./sections/02-intro";
 
 export default function HomeView() {
   return (
     <section>
       <HeroSection />
+      <IntroSection />
     </section>
   );
 }
---
+++ src/view/home/sections/02-intro/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function IntroSection() {
+  return (
+    <section className="font-koho bg-white px-6 py-20">
+      <div className="max-w-7xl mx-auto text-center">
+        <h5 className="text-lg md:text-xl font-medium mb-2 text-black">
+          Indonesia’s Pioneer of Gluten Free food products since 2013, Ladang
+          Lima provides tasty and healthy gluten free foods from sourced local
+          cassava into your table.
+        </h5>
+        <h5 className="text-lg md:text-xl font-medium text-black">
+          Ladang Lima produce cassava flour from local cassava through
+          biotechnology method which have similar characteristic with wheat
+          flour. Not only cassava flour, Ladang Lima also produce derivative
+          products such as Gluten Free Noodles, Cookies, and Pasta for daily
+          tasty dish & delightful snacks.
+        </h5>
+      </div>
+    </section>
+  );
+}
