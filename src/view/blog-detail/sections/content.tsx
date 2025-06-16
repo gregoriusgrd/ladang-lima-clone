b84879eb1d638b2440daeda55917c1c690b5f9a3
Gregorius G <gregorius.gerald21@gmail.com>
2025-06-16 15:12:34 +0700

    remove detail

--- src/view/blog-detail/sections/content.tsx
+++
@@ -1,9 +0,0 @@
-import { IBlog } from "@/interface/blog.interface";
-
-export default function BlogContent({ blog }: { blog: IBlog }) {
-  return (
-    <article>
-      <p>Title: {blog.fields.blogTitle}</p>
-    </article>
-  );
-}
